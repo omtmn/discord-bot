3166aee633d5a308805de57e12ca4b6fb69d8af1
Alex <omtmn@icloud.com>
2021-10-20 13:56:01 -0400

    the bot encourages your negative attitude

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const Discord = require('discord.js')
 const { Client, Intents } = require('discord.js')
 const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
+const sadWords = ['sad', 'depressed', 'unhappy', 'angry', 'upset', 'I cant']
+const encourage = [
+    'You are doing great!',
+    'Remember to stay positive!',
+    'Hang in there.',
+    'You got this!',
+    'Remember to reach out if you ever need anything.',
+    'You are never alone'
+]
+
 dotenv.config()
 
 function getInspired() {
@@ -32,6 +42,11 @@ client.on("message", (msg) => {
 
     if (msg.content === "inspire me") {
         getInspired().then((quote) => {msg.channel.send(quote)})
+    }
+
+    if(sadWords.some(word => msg.content.includes(word))) { // if msg includes a word from sadWords 
+        const encouragement = encourage[Math.floor(Math.random() * encourage.length)] // random encouragement 
+        msg.reply(encouragement)
     }
 })
 
