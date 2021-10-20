9ab34b8588e1c31a54a3293cf016a624972edeff
Alex <omtmn@icloud.com>
2021-10-20 13:31:37 -0400

    the bot inspires you

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const dotenv = require('dotenv')
 const express = require('express')
+// node-fetch was converted to be a ESM only package, so I couldnt use require. async import() used instead
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const server = express()
 const PORT = process.env.PORT || 3000
 
@@ -9,13 +12,26 @@ const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAG
 
 dotenv.config()
 
+function getInspired() {
+    return fetch('https://zenquotes.io/api/random')
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            return data[0]["q"] + " -" + data[0]["a"] // format quote
+            // q is quote, - and then a is the author of the quote
+        })
+}
+
 client.on("ready", () => {
-    console.log(`logged in as ${client.user.tag}!`)
+    console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on("message", (msg) => {
-    if (msg.content === "ping") {
-        msg.reply("pong")
+    if(msg.author.bot) return 
+
+    if (msg.content === "inspire me") {
+        getInspired().then((quote) => {msg.channel.send(quote)})
     }
 })
 
